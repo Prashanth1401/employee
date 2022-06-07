512d47c3a7a4465b0a3efe1d74781d5fc44982be
prashanth <prashanthpatel2580@gmail.com>
2022-06-07 08:37:11 +0530

    updt

--- src/main/java/com/employee/employee_application/App.java
+++ src/main/java/com/employee/employee_application/App.java
@@ -16,7 +16,7 @@ public class App
     public static void main( String[] args )
     {
        EmployeeServiceImpl employeeService = new EmployeeServiceImpl();
-       Employee employee = new Employee(102, "Surya", 10000.0,"JAVA" );
+       Employee employee = new Employee(1401, "prashanth", 10000.0,"BackEnd" );
        Optional<Employee> addedEmpoyee= employeeService.addEmployee(employee);
        if(addedEmpoyee.isPresent()) {
     	   System.out.println("Employee added Success fully");
